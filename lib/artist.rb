60f0c57409c3ee158f0f22c57377dc9756b16c86
Mariam Haider <mfh.haider@gmail.com>
2020-04-13 00:14:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,18 +6,15 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
+    @@all << self 
   end
 
   def add_song(song)
     @songs << song
   end
 
-  def save
-    @@all << self #saves the artist name to the all array
-  end
-
   def self.all
-    @@all #displays the all array
+    @@all 
   end
 
   def self.find_or_create_by_name(artist_name)
