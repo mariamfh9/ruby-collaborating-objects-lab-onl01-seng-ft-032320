61a4241115e2805df08c667715166c89778c5bc7
Mariam Haider <mfh.haider@gmail.com>
2020-04-13 00:14:57 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,15 +6,16 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
+    @@all << self
   end
 
   def add_song(song)
     @songs << song
   end
 
-  def save
-    @@all << self 
-  end
+  #def save
+   # @@all << self 
+  #end
 
   def self.all
     @@all 
