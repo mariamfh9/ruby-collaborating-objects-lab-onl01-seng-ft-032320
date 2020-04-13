337142c63c882cbf4f350437747ac22f94410927
Mariam Haider <mfh.haider@gmail.com>
2020-04-13 00:31:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,6 +13,7 @@ class Song
   end 
 
    def self.new_by_filename(file)
+      artist, song = filename.split(" - ") new_song = self.new(song) new_song.artist_name = artist new_song end
     file.delete_suffix!(".mp3")
     song = file.split(" - ")[1]
     artist = file.split(" - ")[0]
