1e2cc74fd20a3730d668ecd0769662ede5829a91
Mariam Haider <mfh.haider@gmail.com>
2020-04-13 00:37:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,20 +12,16 @@ class Song
     @@all
   end 
 
-def self.new_by_filename(file)
-    song_info = file.chomp(".mp3").split(" - ")
-    song = Song.new(song_info[1])
-    song.artist_name = song_info[0]
-    song
+
+  
+   def self.new_by_filename(file)
+    file.delete_suffix!(".mp3")
+    song = file.split(" - ")[1]
+    artist = file.split(" - ")[0]
+    new_song = self.new(song)
+    new_song.artist_name=(artist)
+    new_song
   end
-   #def self.new_by_filename(file)
-    #file.delete_suffix!(".mp3")
-    #song = file.split(" - ")[1]
-    #artist = file.split(" - ")[0]
-    #new_song = self.new(song)
-    #new_song.artist_name=(artist)
-    #new_song
-  #end
 
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
