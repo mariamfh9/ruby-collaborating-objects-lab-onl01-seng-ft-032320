27c3ba2c241d34c9a3f374f148e1abeb90537132
Mariam Haider <mfh.haider@gmail.com>
2020-04-13 00:14:46 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,11 +6,14 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
-    @@all << self 
   end
 
   def add_song(song)
     @songs << song
+  end
+
+  def save
+    @@all << self 
   end
 
   def self.all
