1db1df7fb1c9cb571f89fc92e02982a37560b493
Mariam Haider <mfh.haider@gmail.com>
2020-04-13 00:12:17 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,7 +20,7 @@ class Artist
   
   
   def self.find_or_create_by_name(artist_name)
-    found_artist = self.all.find {|artist| artist.artist_name == artist_name}
+    found_artist = self.all.find {|artist| artist.name == artist_name}
     if found_artist
       found_artist
     else
