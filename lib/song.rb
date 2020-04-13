f83d4663d2439e05e5c97e1a344eccac69d0558e
Mariam Haider <mfh.haider@gmail.com>
2020-04-13 00:21:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,11 +13,11 @@ class Song
   end 
 
    def self.new_by_filename(file)
-    song = file.split(" - ")[1]
-    artist = file.split(" - ")[0]
-    new_song = self.new(song)
-    new_song.artist_name = artist
-    new_song
+    artist_name, song_name, extra = file.split(" - ")
+    song = self.new(song_name)
+    artist = Artist.find_or_create_by_name(artist_name)
+    artist.add_song(song)
+  end
   end
 
   def artist_name=(name)
